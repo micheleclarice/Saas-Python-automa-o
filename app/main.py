555b22b7fd663099d0d58a8113c2e620e14ea0ad
Michele <micheleclarice@hotmail.com>
2026-04-25 16:29:02 -0300

    Estrutura simplificada com apenas uma pasta app

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, UploadFile, File
-from database import SessionLocal
-from models import Document
-from pdf_loader import extract_pdf
-from crawler import crawl
-from chunking import chunk_text
-from embeddings import embed
-from rag import retrieve
-from agent import SalesAgent
+from app.database import SessionLocal
+from app.models import Document
+from app.pdf_loader import extract_pdf
+from app.crawler import crawl
+from app.chunking import chunk_text
+from app.embeddings import embed
+from app.rag import retrieve
+from app.agent import SalesAgent
 from fastapi.middleware.cors import CORSMiddleware
+from app import models
 
 
 app = FastAPI()
